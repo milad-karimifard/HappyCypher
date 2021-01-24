201364a918c2109218b0cfc1a269962662b0ed56
Milad Karimifard <karimifard.milad@gmail.com>
2021-01-24 14:13:49 +0330

    Add Base Resourse Url

---
+++ src/HappyCypher/Client/Utilities/EndPoints.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HappyCypher.Client.Utilities
+{
+    /// <summary>
+    /// Blockcypher Endpoint list
+    /// you must set api version by SetAPIVersion method
+    /// ex: EndPoints.BitCoinMain.SetAPIVersion("v1");
+    /// </summary>
+    public class EndPoints
+    {
+        #region Coin Base Url 
+
+        public const string BitCoinMain = "{0}/btc/main/";
+        public const string BitCoinTest = "{0}/btc/test3/";
+        public const string DashMain = "{0}/dash/main/";
+        public const string DogeCoinMain = "{0}/doge/main/";
+        public const string LiteCoinMain = "{0}/ltc/main/";
+        public const string BlockCypherTest = "{0}/bcy/test/";
+
+        #endregion
+    }
+}
---
+++ src/HappyCypher/Extensions/EndPoints/SetAPIVersion.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HappyCypher.Extensions.EndPoints
+{
+    public static class EndPointExtension
+    {
+        public static string SetAPIVersion(this string endpoint, string version) => string.Format(endpoint, version);
+    }
+}
